4a279bda191a2db013114295647dac057407eafd
Wasealex <wasealex27@gmail.com>
2022-05-17 08:42:54 +0300

    1st trial

--- _printf.c
+++ _printf.c
@@ -5,17 +5,53 @@
 #include <unistd.h>
 
 /**
-  * find_function - function that finds formats for _printf
-  * calls the corresponding function.
-  * @format: format (char, string, int, decimal)
-  * Return: NULL or function associated ;
-  */
-/**
  * _printf - write output to stdout, the standard output stream
  * @format:  is a character string composed of zero or more directives
  * Return: the number of characters printed (excluding the null)
  */
 int _printf(const char *format, ...)
 {
+	int i = 0, j = 0, output = 0;
+	va_list arg;
+
+	cf_t print[] = {
+		{"c", print_c},
+		{"s", print_s},
+		{NULL, NULL}
+	};
+
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+		return (-1);
+	va_start(arg, format);
+	while (format[i] != '\0')
+	{
+		if (format[i] == '%' && format[i + 1] != '%')
+		{
+			j = 0;
+
+			while (print[j].p != NULL)
+			{
+				if (format[j + 1] == print[j].print[0])
+				{
+					output = output + print[j].p(arg);
+					i++;
+				}
+				j++;
+			}
+		}
+		else if (format[i] == '%' && format[i + 1] == '%')
+		{
+			_putchar ('%');
+			i++;
+			output = output + 1;
+		}
+		else
+		{
+			_putchar (format[i]);
+			output = output + 1;
+		}
+		i++;
+	}
+	va_end(arg);
 	return (output);
 }
--- main.h
+++ main.h
@@ -6,3 +6,23 @@
 #include <stddef.h>
 #include <stdlib.h>
 #include <unistd.h>
+
+/**
+ *struct cformat - structure for the format
+ *@print: - conversion specifiers
+ *@p: - point to funcion
+ */
+
+typedef struct cformat
+{
+char *print;
+int (*p)();
+} cf_t;
+
+int _printf(const char *format, ...);
+int _putchar(char c);
+int print_c(va_list arg);
+int print_s(va_list arg);
+;
+
+#endif
