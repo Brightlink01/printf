21beb1b3f9e3a578a457c48ff90be7e99920e08f
Wasealex <wasealex27@gmail.com>
2022-05-17 07:43:37 +0300

    startover

--- _printf.c
+++ _printf.c
@@ -5,67 +5,17 @@
 #include <unistd.h>
 
 /**
+  * find_function - function that finds formats for _printf
+  * calls the corresponding function.
+  * @format: format (char, string, int, decimal)
+  * Return: NULL or function associated ;
+  */
+/**
  * _printf - write output to stdout, the standard output stream
  * @format:  is a character string composed of zero or more directives
  * Return: the number of characters printed (excluding the null)
  */
 int _printf(const char *format, ...)
 {
-	va_list arg;
-	char cha;
-	int output = 0, i = 0;
-
-	va_start(arg, format);
-	for (; format && format[i]; i++)
-	{
-		for (; format[i] && format[i] != '%'; i++, output++)
-			_putchar(format[i]);
-		if (!format[i])
-			return (output);
-		else if (format[i] == '%')
-		{
-			i++;
-			cha = format[i];
-			if (cha == '%')
-				_putchar('%');
-			else
-				output += _conv(arg, cha);
-			output++;
-		}
-		if (!format[i])
-			return (output);
-	}
-	va_end(arg);
-
-	return (output);
-}
-
-/**
- *_conv - handle the following conversion specifiers
- *@list: the argument list
- *@ch: character to process
- *
- *Return: number of characters printed
- */
-void _conv(va_list list, char ch)
-{
-	int j = 0;
-	int output = 0;
-	print p[] = {
-		{ 'c', print_c },
-		{ 's', print_s },
-		{ 0, NULL}
-	};
-
-	while (p[j].ch)
-	{
-		if (p[j].ch == ch)
-		{
-			output += p[j].print(list);
-			break;
-		}
-		++j;
-	}
-
 	return (output);
 }
--- main.h
+++ main.h
@@ -6,24 +6,3 @@
 #include <stddef.h>
 #include <stdlib.h>
 #include <unistd.h>
-
-/**
- *struct printing - structure the conversion specifier
- *
- *@ch: conversion
- *@prints: a pointer to too function for the argument list
- *return intiger
- */
-typedef struct printing
-{
-	char ch;
-	int (*prints)(va_list list);
-} print_t;
-
-int _putchar(char c);
-int _printf(const char *format, ...);
-int print_c(va_list arg);
-int print_s(va_list arg);
-int print_p(va_list arg __attribute__((unused)));
-
-#endif
