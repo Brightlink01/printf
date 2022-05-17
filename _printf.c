7388c0ddda85d8483ef4358fdf2122382c757651
Brightlink01 <Brightlink01@gmail.com>
2022-05-17 10:22:31 -0700

    committing tasks

--- _printf.c
+++ _printf.c
@@ -1,70 +1,39 @@
 #include "main.h"
-#include <stddef.h>
-#include <stdarg.h>
-#include <stddef.h>
-#include <unistd.h>
 
 /**
- *_conv - process conversion specifier
- *@arg: list of variable argument
- *@ch: character process
- *Return: number of characters printed
+ * _printf - Receives the main string and all the necessary parameters to
+ * print a formated string
+ * @format: A string containing all the desired characters
+ * Return: A total count of the characters printed
  */
 
-int _conv(va_list arg, char ch)
-{
-	int j = 0;
-	int output = 0;
-	printer_t p[] = {
-		{'c', print_c},
-		{'s', print_s},
-		{0, NULL}
-	};
-	while (p[j].ch)
-	{
-		if (p[j].ch == ch)
-		{
-			output += p[j].print(arg);
-			break;
-		}
-		++j;
-	}
-	return (output);
-}
-
-
-/**
- * _printf - write output to stdout, the standard output stream
- * @format:  is a character string composed of zero or more directives
- * Return: the number of characters printed (excluding the null)
- */
 int _printf(const char *format, ...)
 {
-	va_list arg;
-	int output = 0, i = 0;
-	char c;
+	int printed_chars;
+	conver_t f_list[] = {
+		{"%", print_percent},
+		{"d", print_integer},
+		{"i", print_integer},
+		{"c", print_char},
+		{"s", print_string},
+		{"b", print_binary},
+		{"u", print_unsigned_integer},
+		{"o", print_octal},
+		{"x", print_hex},
+		{"X", print_HEX},
+		{"S", print_String},
+		{"p", print_pointer},
+		{"r", print_rev},
+		{"R", print_rot13},
+		{NULL, NULL},
+	};
+	va_list arg_list;
 
-	va_start(arg, format);
+	if (format == NULL)
+		return (-1);
 
-	for (; *format && *(format + i); i++)
-	{
-		for (; *(format + i) && *(format + i) != '%'; i++, output++)
-			_putchar(*(format + i));
-		if (!format[i])
-			return (output);
-		else if (*(format + i) == '%')
-		{
-			i++;
-			c = *(format + i);
-			if (c == '%')
-				_putchar('%');
-			else
-				output += _conv(arg, c);
-			output++;
-		}
-		if (!format[i])
-			return (output);
-	}
-	va_end(arg);
-	return (output);
+	va_start(arg_list, format);
+	printed_chars = format_reciever(format, f_list, arg_list);
+	va_end(arg_list);
+	return (printed_chars);
 }
--- main.h
+++ main.h
@@ -2,28 +2,55 @@
 #define MAIN_H
 
 #include <stdio.h>
-#include <stdarg.h>
-#include <stddef.h>
-#include <stdlib.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <limits.h>
+
+#define NULL_STRING "(null)"
+#define NUL '\0'
 
 /**
- *struct printer - structure for the conversion specifier
- *@ch: format specifier
- *@print: pointer to function takes variable retun intiger
+ * struct convert - defines a structure for symbols and functions
  *
+ * @sym: The operator
+ * @f: The function associated
  */
-typedef struct printer
-{
-	char ch;
-	int (*print)(va_list arg);
-} printer_t;
 
-int _putchar(char c);
+struct convert
+{
+	char *sym;
+	int (*f)(va_list);
+};
+typedef struct convert conver_t;
+
+
 int _printf(const char *format, ...);
-int _conv(va_list arg, char ch);
-int print_c(va_list arg);
-int print_s(va_list arg);
-int print_p(va_list arg __attribute__((unused)));
-int _puts(char *stg);
+int _putchar(char c);
+int format_reciever(const char *format, conver_t f_list[], va_list arg_list);
+int print_percent(va_list);
+int print_integer(va_list);
+int print_char(va_list);
+int print_string(va_list);
+int print_binary(va_list);
+int print_unsigned_integer(va_list);
+int print_octal(va_list list);
+int print_hex(va_list list);
+int print_HEX(va_list list);
+int print_String(va_list val);
+int print_pointer(va_list val);
+int print_rev(va_list l);
+int print_rot13(va_list list);
+
+int print_number(va_list args);
+unsigned int base_len(unsigned int, int);
+char *rev_string(char *);
+void write_base(char *str);
+char *_memcpy(char *dest, char *src, unsigned int n);
+int print_unsgined_number(unsigned int n);
+int hex_check(int num, char x);
+int print_hex_aux(unsigned long int num);
+int isNonAlphaNumeric(char c); 
+int _puts(char *str);
+char *convert(unsigned long int num, int base, int lowercase);
 #endif
